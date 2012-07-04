0c3bfb6943e499612f0d5b68e0ded333b89587b5
marius <mmillea@ucdavis.edu>
2012-07-03 18:33:22 -0700

    added cobyla

--- pycosmc/samplers/cobyla.py
+++ pycosmc/samplers/cobyla.py
@@ -33,23 +33,23 @@ class cobyla(Sampler):
         
         yield xopt, 1, lnl(xopt,p)[0], lnl(xopt,p)[1]
         
-        def hess(f,x,dx):
-            def xdx(fi,i,fj,j):
-                y=list(x)
-                y[abs(i)]+=fi*dx[abs(i)]
-                y[abs(j)]+=fj*dx[abs(j)]
-                return f(y)
-            n=len(x)
-            h=zeros((n,n))
-            for i in range(n):
-                for j in range(i,n):
-                    h[i,j]=h[j,i]= (xdx(1,i,1,j)-xdx(1,i,-1,j)-xdx(-1,i,1,j)+xdx(-1,i,-1,j))/(4*dx[i]*dx[j])
-            return h
-
-        H = hess(lnl_unscaled,xopt,scale/100)
-        print H
-            
-#        from numdifftools import Hessian
-#        
-#        iH = inv(Hessian(lnl_unscaled,stepNom=scale/100.)(xopt))
-        print dict(zip(['.'.join(k) for k in p.get_all_sampled().keys()],sqrt(diag(inv(H)))))
+#        def hess(f,x,dx):
+#            def xdx(fi,i,fj,j):
+#                y=list(x)
+#                y[abs(i)]+=fi*dx[abs(i)]
+#                y[abs(j)]+=fj*dx[abs(j)]
+#                return f(y)
+#            n=len(x)
+#            h=zeros((n,n))
+#            for i in range(n):
+#                for j in range(i,n):
+#                    h[i,j]=h[j,i]= (xdx(1,i,1,j)-xdx(1,i,-1,j)-xdx(-1,i,1,j)+xdx(-1,i,-1,j))/(4*dx[i]*dx[j])
+#            return h
+#
+#        H = hess(lnl_unscaled,xopt,scale/100)
+#        print H
+#            
+##        from numdifftools import Hessian
+##        
+##        iH = inv(Hessian(lnl_unscaled,stepNom=scale/100.)(xopt))
+#        print dict(zip(['.'.join(k) for k in p.get_all_sampled().keys()],sqrt(diag(inv(H)))))
