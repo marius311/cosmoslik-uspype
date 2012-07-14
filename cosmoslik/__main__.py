25549c7ed2f655caa83d2036bae3866f6fa59e47
marius <mmillea@ucdavis.edu>
2012-07-14 03:57:28 -0700

    renamed in uspype

---
+++ cosmoslik/__main__.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+
+import sys, os, cosmoslik
+import argparse
+
+parser = argparse.ArgumentParser(prog='cosmoslik.py')
+parser.add_argument('params.ini',nargs='?',help='a parameter file to run')
+parser.add_argument('--list',action='store_true',default=False,help='list available modules')
+parser.add_argument('--doc',nargs=1,metavar='<module>',help='print the documentation for a module')
+parser.add_argument('--html_doc',nargs=1,metavar='<module>',help='open the documentation for a module in a web-browser')
+parser.add_argument('--build',nargs='?',metavar='<modules>',default=False,help='run build script for a module (default: all modules)')
+parser.add_argument('-n',nargs=1,metavar='<# of chains>',default=False,help='run multiple chains with MPI')
+parser.add_argument('--qsub',action='store_true',default=False,help='submit via qsub')
+
+
+if not sys.argv[1:]: parser.print_help()
+else:
+    
+    args = vars(parser.parse_args())
+    
+    if args['list']:
+        import pkgutil
+        pkgs = ['cosmoslik.likelihoods', 'cosmoslik.derivers', 'cosmoslik.models', 'cosmoslik.samplers']
+        print "Found the following modules:"
+        for p in pkgs:
+            for _, modname, _ in pkgutil.iter_modules(__import__(p,fromlist=[p.split('.')[1]]).__path__):
+                print '  %s.%s'%(p.split('.')[1],modname)
+        print "See 'cosmoslik.py --doc <module>' for more information on a given module."
+        
+    elif args['doc'] or args['html_doc']:
+        from textwrap import dedent
+        modname = (args['doc'] or args['html_doc'])[0]
+        try:
+            mod = __import__('cosmoslik.%s'%modname,fromlist=[modname.split('.')[-1]])
+        except ImportError:
+            print "'%s' module not found.\nSee 'cosmoslik.py --list' to list all available modules."%modname
+        else:
+            doc = mod.__getattribute__(modname.split('.')[-1]).__doc__
+            if args['doc']:
+                print "Documentation for module '%s':"%modname
+                print dedent(doc)
+            else:
+                from docutils.core import publish_string
+                from tempfile import mktemp
+                import webbrowser
+                tmpfile = mktemp(suffix='.html')
+                with open(tmpfile,'w') as f: f.write(publish_string(dedent(doc),writer_name='html'))
+                webbrowser.open(tmpfile)
+
+            
+    elif args['build'] is not False:
+        cosmoslik.build(args['build'])
+        
+    elif args['qsub']:
+        from subprocess import Popen, PIPE
+        from cosmoslik import params
+
+        inifile = args['params.ini']
+        
+        if ('camb' in params.read_ini(inifile).get('models',[])): nnodes, ppn, wall, nproc = 6, 1, 24, 7
+        else: nnodes, ppn, wall, nproc = 1, 1, 6, 9
+        
+        name = inifile.replace('cosmoslik.','').replace('params.','').replace('.ini','')
+        dir = os.path.dirname(os.path.abspath(inifile))
+        sys.argv.remove('--qsub')
+        
+        proc = Popen(["qsub","-q","usplanck","-l",
+                      "nodes=%s:ppn=%s,pvmem=20gb"%(nnodes,ppn),
+                      "-l","walltime=%s:00:00"%wall,
+                      "-N",name,"-o","%s.log"%name,"-j","oe","-V"],stdin=PIPE,stdout=PIPE)
+        
+        proc.stdin.write('cd %s && %s -m cosmoslik -n %i %s'%(dir,sys.executable,nproc,' '.join(sys.argv[1:])))
+        proc.stdin.close()
+        print proc.stdout.readline()
+
+    elif args['n']:
+        i = sys.argv.index('-n')
+        sys.argv.pop(i); sys.argv.pop(i)
+        os.system("mpiexec -n %i %s -m cosmoslik %s"%(int(args['n'][0]),sys.executable,' '.join(sys.argv[1:])))
+        
+    elif args['params.ini']:
+        for _ in cosmoslik.sample(args['params.ini']): pass
+
---
+++ cosmoslik/samplers/metropolis_hastings.py
@@ -0,0 +1,125 @@
+from numpy import log, mean, array, sqrt, diag, genfromtxt, sum, dot, cov, inf
+from random import random
+from numpy.random import multivariate_normal
+import cosmoslik.mpi as mpi, re, time
+from itertools import product
+from collections import namedtuple
+from cosmoslik.modules import Sampler
+    
+sampletuple = namedtuple('sampletuple',['x','weight','lnl','extra'])
+    
+
+class metropolis_hastings(Sampler):
+    
+    def init(self,p):
+        if mpi.get_rank()>0: 
+            if 'output_file' in p: p['output_file']+=('_%i'%mpi.get_rank())
+            if 'samples' in p: p['samples']/=(mpi.get_size()-1)
+            p['quiet']=True 
+        elif mpi.get_size()>1: 
+            p.pop('output_file')
+        
+        
+    def sample(self,x,lnl,p):
+        """
+        Returns a generator which yields samples from the likelihood 
+        using the Metropolis-Hastings algorithm.
+        
+        The samples returned are tuples of (x,weight,lnl,extra)
+            lnl - likelihood
+            weight - the statistical weight (could be 0 for rejected steps)
+            x - the vector of parameter values
+            extra - the extra information returned by lnl
+        """
+        
+        if mpi.get_size()==1: return _mcmc(x,lnl,p)
+        else: return _mpi_mcmc(x,lnl,p)
+    
+    
+    
+def _mcmc(x,lnl,p):
+   
+    cur_lnl, cur_weight, cur_x = inf, 0, x
+
+    def update(v): 
+        if v!=None: p.update(v)
+        
+    for _ in range(p.get("samples",100)):
+        test_x = multivariate_normal(cur_x,p['_cov']/len(x)*p.get('proposal_scale',2.4)**2)
+        test_lnl, test_extra = lnl(test_x, p)
+                
+        if (log(random()) < cur_lnl-test_lnl):
+            if cur_lnl!=inf: update((yield(sampletuple(cur_x, cur_weight, cur_lnl, cur_extra))))
+            cur_lnl, cur_weight, cur_x, cur_extra = test_lnl, 1, test_x, test_extra
+        else:
+            if p.get('metropolis_hastings',{}).get('weighted_samples',True): cur_weight+=1
+            else: update((yield(sampletuple(cur_x, cur_weight, cur_lnl, cur_extra))))
+            
+            if p.get('metropolis_hastings',{}).get('rejected_samples',True): 
+                update((yield(sampletuple(test_x, 0, test_lnl, test_extra))))
+            
+            
+            
+def _mpi_mcmc(x,lnl,p):  
+
+    (rank,size,comm) = mpi.get_mpi()
+    from mpi4py import MPI #FIX THIS
+    
+    if rank==0:
+        finished = [False]*(size-1)
+        samples, weights, lnls = [[[] for _ in range(size-1)] for __ in range(3)] 
+        while (not all(finished)):
+            while not comm.Iprobe(source=MPI.ANY_SOURCE, tag=0): time.sleep(.1) #Hack so OpenMPI doesn't eat 100% CPU
+            (source,new_samples)=comm.recv(source=MPI.ANY_SOURCE)
+            if (new_samples!=None): 
+                lnls[source-1]+=[s.lnl for s in new_samples]
+                samples[source-1]+=[s.x for s in new_samples]
+                weights[source-1]+=[s.weight for s in new_samples]
+                
+                if (p.get("proposal_update",True) 
+                    and sum(weights[source-1])>p.get('proposal_update_start',1000)):
+                    comm.send({"_cov":get_new_cov(samples,weights)},dest=source)
+                else: comm.send({},dest=source)
+                comm.send(None,dest=source)
+
+            else: 
+                finished[source-1]=True
+                
+    else:
+        samples = []
+        sampler = _mcmc(x, lnl, p)
+        s=sampler.next()
+        while True:
+            try:
+                yield s
+                samples.append(sampletuple(s.x,s.weight,s.lnl,None))
+                if len(samples)==50:
+                    comm.send((rank,samples),dest=0)
+                    s = sampler.send(comm.recv(source=0))
+                    samples = []
+                else: s = sampler.next()
+            except StopIteration: break
+        comm.send((rank,None),dest=0)
+
+       
+def get_covariance(data,weights=None):
+    if (weights==None): return cov(data.T)
+    else:
+        mean = sum(data.T*weights,axis=1)/sum(weights)
+        zdata = data-mean
+        return dot(zdata.T*weights,zdata)/(sum(weights)-1)
+
+
+def get_new_cov(samples,weights=None):
+    """
+    shape(samples) = (nchains,nsamples,nparams)
+    shape(weights) = (nchains,nsamples)
+    """
+    data = array(reduce(lambda a,b: a+b,[s[len(s)/2:] for s in samples]))
+    weights = array(reduce(lambda a,b: a+b,[w[len(w)/2:] for w in weights]))
+    return get_covariance(data, weights)
+    
+    
+def gelman_rubin_R(samples):
+    return 1
+    
