a20cd3f5138eec1d93560fe4f9a704eaa2f50fbd
marius <mmillea@ucdavis.edu>
2012-07-30 03:08:09 -0700

    spt build scripts

--- cosmoslik/likelihoods/spt_r11/setup.py
+++ cosmoslik/likelihoods/spt_r11/setup.py
@@ -1,6 +1,6 @@
 import os
 
 if not os.path.exists('spt_multif_0809'):
-    os.system("wget http://lambda.gsfc.nasa.gov/data/suborbital/SPT/spectra_2011/bandpowers_spt20082009.tar.gz")
-    os.system("tar zxvf bandpowers_spt20082009.tar.gz")
+    os.system("wget http://pole.uchicago.edu/public/data/reichardt11/bandpowers_spt_multif_0809.tar.gz")
+    os.system("tar zxvf bandpowers_spt_multif_0809.tar.gz")
 
