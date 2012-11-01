f92a735c6f15ec6813ee07818d848491e6296080
marius <mmillea@ucdavis.edu>
2012-11-01 04:50:45 -0700

    slik camspecs

---
+++ cosmoslik/plugins/likelihoods/camspec_slik.py
@@ -0,0 +1,55 @@
+from cosmoslik.plugins import Likelihood
+from numpy import hstack, arange, array, cumsum, frombuffer, dot, ix_
+from scipy.linalg import cho_factor, cho_solve, inv
+import cPickle
+
+class camspec_slik(Likelihood):
+    
+    def init(self,p):
+        
+        self.labels = [(100,100),(143,143),(217,217),(143,217)]        
+        self.freqs = {x:x for x in self.labels}
+        self.in_lrange = [(50,1201),(50,2001),(500,2501),(500,2501)]
+        self.nl = [u-l for u,l in self.in_lrange]
+        self.ells = hstack([arange(*r) for r in self.in_lrange])
+        self.out_lrange = p.get(('camspec','lrange'),self.in_lrange)
+        
+        for ri,ro in zip(self.in_lrange, self.out_lrange):
+            if ro and (ro[0]<ri[0] or ro[1]>ri[1]): 
+                raise Exception("Camspec lrange outside of available range.")
+        
+        self.slice = hstack([arange(*(array(r)+s-l)) 
+                             for s,(l,u),r in zip([0]+list(cumsum(self.nl)[:-1]),
+                                                  self.in_lrange,
+                                                  self.out_lrange)
+                             if r])
+        
+        with open(p['camspec','like_file'],'r') as f: self.x, cv = cPickle.load(f)
+        self.x *= (self.ells*(self.ells+1))
+        cv = ((cv*self.ells*(self.ells+1)).T*self.ells*(self.ells+1)).T
+        self.cho_cov = cho_factor(cv[ix_(self.slice,self.slice)])
+        
+        
+    def get_required_models(self, model):
+        return ['cl_TT','egfs']
+        
+    def lnl(self, p, model):
+        dcl = self.x[self.slice] - hstack(self.get_cl_model(p, model))[self.slice]
+        return dot(dcl,cho_solve(self.cho_cov,dcl))/2
+    
+    def get_cl_model(self, p, model):
+        return [model['cl_TT'][slice(*r)] + 
+                model['egfs']('cl_TT',lmax=r[1],freqs=self.freqs[l])[slice(*r)] 
+                for r,l in zip(self.in_lrange, self.labels)]
+            
+#    def plot(self, p, ax=None):
+#        from matplotlib.pyplot import figure
+#        if ax is None: ax=figure().add_subplot(111)
+#        
+#        ax.errorbar(arange(*self.lrange),
+#                    self.spec[self.lslice],
+#                    yerr=self.errorbars[self.lslice])
+#        
+#        ax.plot(arange(*self.lrange), self.get_cl_model(p, p['_model']))
+#        
+        
---
+++ cosmoslik/plugins/models/planck_egfs/camspec.py
@@ -0,0 +1,50 @@
+from cosmoslik.plugins.models.egfs import egfs
+from numpy import arange, hstack, loadtxt, zeros, exp, sqrt, pi, mean
+import os
+
+class camspec(egfs):
+    
+    def init(self, p): 
+        self.norm_ell = float(p.get(('egfs','norm_ell'),1500))
+        
+        self.dir = os.path.dirname(os.path.abspath(__file__))
+        padding = zeros(10000)
+        def todl(cl,norm=self.norm_ell): return (lambda dl: dl/dl[norm])((lambda l: cl*l*(l+1)/2/pi)(arange(cl.shape[0])))
+        self.tsz_template = todl(hstack([[0,0],loadtxt(os.path.join(self.dir,"tsz.dat"))[:,1],padding]))
+        self.ksz_template = todl(hstack([[0,0],loadtxt(os.path.join(self.dir,"ksz_ov.dat"))[:,1],padding]))
+
+    def get_egfs(self, p, spectra, lmax, freqs, **kwargs):
+        
+        freqs = tuple(fr if isinstance(fr,dict) else {'tsz':fr} for fr in freqs)
+        
+        frlbl = tuple([n for n,(l,u) in {100:(80,120),
+                                         143:(130,160), 
+                                         217:(200,240)}.items() 
+                       if l<mean(fr.values())<u][0] for fr in freqs)
+        
+        comps={}
+        
+        p = p['egfs']
+        comps['tsz'] = p['a_tsz'] * self.tsz_template[:lmax] * tszdep(freqs[0]['tsz'],freqs[1]['tsz'], p['tsz_norm_fr']) 
+        comps['ksz'] = p['a_ksz'] * self.ksz_template[:lmax] 
+        
+        if frlbl==(100,100):
+            comps['ps_100'] = p['a_ps_100']*(arange(lmax)/self.norm_ell)**2
+        elif frlbl==(143,143):
+            comps['ps_143'] = p['a_ps_143']*(arange(lmax)/self.norm_ell)**2
+            comps['cib_143'] = p['a_cib_143']*(arange(lmax)/self.norm_ell)**0.7
+        elif frlbl==(217,217):
+            comps['ps_217'] = p['a_ps_217']*(arange(lmax)/self.norm_ell)**2
+            comps['cib_217'] = p['a_ps_217']*(arange(lmax)/self.norm_ell)**0.7
+        elif sorted(frlbl)==(143,217):
+            comps['ps_143_217'] = p['r_ps']*sqrt(p['a_ps_143']*p['a_ps_217'])*(arange(lmax)/self.norm_ell)**2
+            comps['cib_143_217'] = p['r_cib']*sqrt(p['a_cib_143']*p['a_cib_217'])*(arange(lmax)/self.norm_ell)**0.7
+            
+        return comps
+            
+        
+def tszdep(fr1,fr2,fr0):
+    """The tSZ frequency dependence."""
+    t1,t2,t0 = map(lambda fr: (lambda x0: x0*(exp(x0)+1)/(exp(x0)-1) - 4)(fr/56.78),[fr1,fr2,fr0])
+    return t1*t2/t0**2
+        
